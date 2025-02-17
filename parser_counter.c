910c866905dc98a8de025ee9f46855101c5d5db7
golina123 <142676841+golina123@users.noreply.github.com>
2025-02-17 22:52:43 +0300

    Update parser_counter.c

--- parser_counter.c
+++ parser_counter.c
@@ -8,34 +8,39 @@
 //#define del" .,?!"
 struct parser
 {
-	FILE * fp;
+        FILE * fp;
 };
 
 struct parser * parser_create()
 {
-	struct parser *p = malloc(sizeof(struct parser));
-	if(p == NULL)
-	{
-	   perror("storage error ");
+        struct parser *p = malloc(sizeof(struct parser));
+        if(p == NULL)
+        {
+           perror("storage error ");
 
         }
-	return p;
+        return p;
 }
 
 
-void parser_getword(struct parser *p, const char *fi)
+char *parser_getword(struct parser *p, const char *fi)
 {
-	p->fp = fopen(fi, "r");
-	char word[4096];
-	while((fscanf(p->fp, "%s", word ) != EOF))
-	{
-		printf("%s\n", word);
-	}
+        p->fp = fopen(fi, "r");
+        char word[4096];
+        while((fscanf(p->fp, "%s", word ) != EOF))
+        {
+                char *result = strdup(word);
+                fclose(p->fp);
+                return result;
+        }
+
+    fclose(p->fp);
+    return NULL;
+
 
 }
 
 void parser_delete(struct parser*p)
 {
-	fclose(p->fp);
-	free(p);
+        free(p);
 }
