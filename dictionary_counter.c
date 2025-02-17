57adc65f25736e7f8d3fd52e033bbdc3885e2f84
golina123 <142676841+golina123@users.noreply.github.com>
2025-02-17 15:21:20 +0300

    Update dictionary_counter.c

--- dictionary_counter.c
+++ dictionary_counter.c
@@ -1,59 +1,60 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 #include<string.h>
-#define MAXSIZE = 1000
+#define MAXSIZE 1000
 struct element
 {
         char *word;
-	int rang;
+        int rang;
 
 };
 
 struct dictionary {
-	struct element a[MAXSIZE];
-	int count;
+        struct element a[MAXSIZE];
+        int count;
 };
 
 struct dictionary * dictionary_create()
 {
-	struct dictionary *d  = malloc(sizeof(struct dictionary));
-	if(d == NULL)
+        struct dictionary *d  = malloc(sizeof(struct dictionary));
+        if(d == NULL)
         {
            perror("storage error ");
 
         }
-	d->count = 0;
-	return d;
+        d->count = 0;
+        return d;
 }
 
 void dictionary_add_word(struct dictionary *d, const char * word )
 {
-	
-	int c = 0, i = 0, n = 100;
-	for(i =  0; i < d->count; i++){
+
+        int i = 0;
+        for(i =  0; i < d->count; i++){
             if(strcmp (word, d ->a[i].word) == 0)
-	    {
-		    d->a[i].rang += 1;
-		    break;
-	    }
-	}
-	if(i == d->count)
-	{
-		 d->a[i].word = strdup(word);
-		 d->a[i].rang = 1;
-		 d->count ++;
-	}
+            {
+                    d->a[i].rang += 1;
+                    break;
+            }
+        }
+        if(i == d->count)
+        {
+                 d->a[i].word = strdup(word);
+                 d->a[i].rang = 1;
+                 d->count ++;
+        }
 
 
 }
 void dictionary_print(struct dictionary *d)
 {
-	for(int i = 0; i < n; i++){
-		printf("%c %d \n", d->a[i].word, d->a[i].rang );
-	}
+        for(int i = 0; i < d->count; i++){
+                printf("%s %d \n", d->a[i].word, d->a[i].rang );
+        }
 }
 void dictionary_delete(struct dictionary*d)
 {
-        
-        free(d); 
+
+        free(d);
 }
