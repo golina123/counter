98dc630709e371de4d2551c5d3b613a957620f1b
golina123 <142676841+golina123@users.noreply.github.com>
2025-02-17 22:55:28 +0300

    Update dictionary.h

--- dictionary.h
+++ dictionary.h
@@ -1,6 +1,6 @@
 struct element;
 struct dictionary;
 struct dictionary * dictionary_create();
-void dictionary_add_word(struct, const char);
-void dictionary_delete(struct);
-void dictionary_print(struct);
+void dictionary_add_word();
+void dictionary_delete();
+void dictionary_print();
