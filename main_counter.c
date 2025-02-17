0892cac47f0795d1f593bcc12d68f274b4196455
golina123 <142676841+golina123@users.noreply.github.com>
2025-02-17 22:54:36 +0300

    Update main_counter.c

--- main_counter.c
+++ main_counter.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
 #include"dictionary.h"
 #include"parser.h"
-int main(int argv , char * argc[])
+int main(int argc , char * argv[])
 {
-        FILE * Text;
-        struct parser a;
-        parser * p = parser_create();
-        dictionary * d = dictionary_create();
 
         char * word;
-        while( (word = parser_getword(p)) != NULL)
+        struct parser * p = parser_create();
+        struct dictionary * d = dictionary_create();
+
+
+        while( (word = parser_getword(p, argv[1])) != NULL)
         {
                  dictionary_add_word(d, word);
         }
         parser_delete(p);
 
         dictionary_print(d);
-        dictionary_delete(d)
+        dictionary_delete(d);
         return 0;
-
-
-
 }
