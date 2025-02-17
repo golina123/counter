fd4fb4228a86b511af8fcd5d89c34772e4233fa4
golina123 <142676841+golina123@users.noreply.github.com>
2025-02-17 22:53:33 +0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,4 +1,4 @@
 struct parser;
 struct parser * parser_create();
-void parser_getword(struct, const char);
-void parser_delete(struct);
+char *parser_getword(struct parser *p, const char *fi);
+void parser_delete();
